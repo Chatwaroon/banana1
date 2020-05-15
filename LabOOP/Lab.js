245aece78053d0ccb661a751b79d6fc6521dc5da
chatwaroon244733 <chatwaroon_b@kkumail.com>
2020-05-15 20:35:05 +0700

    new commit

---
+++ LabOOP/Lab.js
@@ -0,0 +1,17 @@
+class UserTemplate{
+constructor(name,password,age,date){
+    this.name=name
+    this.password=password
+    this.age=age
+    this.date=date
+    this.connected=false   
+}
+login(){
+    this.connected = true
+    console.log('your login status:',this.connected)
+}
+logout(){
+    this.connected = false
+    console.log('your logout status:',this.connected)
+}
+}
